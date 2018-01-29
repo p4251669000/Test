4ed62d74955208f7fd517d714ce44b0699835348
xier <v-xier@vanke.com>
2018-01-30 01:00:32 +0800

    fx2 class

---
+++ test/fx2.cs
@@ -0,0 +1,18 @@
+﻿/*================================================================
+ * 说明：
+ * 创建人：谢锐
+ * 创建时间 ：2018/1/30 1:00:09
+ *================================================================
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class fx2
+    {
+    }
+}
